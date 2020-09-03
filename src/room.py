39b7f52e90251201c97c9345c61ff10c4cf95f79
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-09-03 15:11:47 -0600

    getting started on the Day 2 MVP, implementing items in rooms the player can collect

--- src/room.py
+++ src/room.py
@@ -5,3 +5,20 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+
+class Item:
+    def __init__(self):  # what things could this have? properties?
+        pass
+
+
+# Weapon is_a Item
+class Weapon(Item):
+    def __init__(self):  # what properties would we use here? damage, powers, etc..?
+        pass
+
+
+# Treasure is_a Item
+class Treasure(Item):
+    def __init__(self):  # what properties could you have in this init function?
+        pass
